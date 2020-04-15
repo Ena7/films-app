2fe6fe48ec272da0f7e9e273665b2bc6e26dcf2d
Enache Alexandru <63500798+Ena7@users.noreply.github.com>
2020-04-15 14:29:58 +0300

    Added comment

--- films-app-v3/valid.h
+++ films-app-v3/valid.h
@@ -18,5 +18,8 @@ ostream& operator<<(ostream& out, const ValidException& ex);
 
 class Validator {
 public:
+
+	//valideaza un film
+	//exceptie daca: titlu este vid, genul este vid, actorul este vid, anul este mai mic decat 1800
 	void validate(const Film& film);
 };
